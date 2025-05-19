d1d2e4207e4cbca096de18e660043af1eaf96dff
tiger <ecustbaihu@163.com>
2025-05-19 21:35:51 +0800

    feat:  modify request freq

--- app/remote_request.py
+++ app/remote_request.py
@@ -1,6 +1,6 @@
 from time import sleep
 import requests
-
+from  datetime import datetime
 
 url = "https://codegeneratemoney.onrender.com/"
 
@@ -8,8 +8,8 @@ if __name__ == '__main__':
     while True:
         response = requests.get(url)
         if response.status_code == 200:
-            print(response.text)
+            print(datetime.now(),response.text)
         else:
             print(response.status_code)
-        sleep(10)
+        sleep(2*60)
     pass
